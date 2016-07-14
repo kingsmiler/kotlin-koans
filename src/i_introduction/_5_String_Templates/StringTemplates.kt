66c131581d5ec367663d29719a4c454b5610eefb
kingsmiler <kingsmiler@163.com>
2016-07-14 19:51:54 +0800

    update my koans test

--- src/i_introduction/_5_String_Templates/StringTemplates.kt
+++ src/i_introduction/_5_String_Templates/StringTemplates.kt
@@ -1,13 +1,7 @@
 package i_introduction._5_String_Templates
 
-import util.TODO
-import util.doc5
-
 fun example1(a: Any, b: Any) =
         "This is some text in which variables ($a, $b) appear."
-
-fun example2(a: Any, b: Any) =
-        "You can write it in a Java way as well. Like this: " + a + ", " + b + "!"
 
 fun example3(c: Boolean, x: Int, y: Int) = "Any expression can be used: ${if (c) x else y}"
 
@@ -25,14 +19,7 @@ fun example() = "13.06.1992".matches(getPattern().toRegex()) //true
 
 val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
 
-fun todoTask5(): Nothing = TODO(
-    """
-        Task 5.
-        Copy the body of 'getPattern()' to the 'task5()' function
-        and rewrite it in such a way that it matches '13 JUN 1992'.
-        Use the 'month' variable.
-    """,
-    documentation = doc5(),
-    references = { getPattern(); month })
-
-fun task5(): String = todoTask5()
+fun task5(): String {
+    // 字符串模板，可在字符串中直接使用变量，与SHELL脚本类似。
+    return """\d{2}\s$month\s\d{4}"""
+}
